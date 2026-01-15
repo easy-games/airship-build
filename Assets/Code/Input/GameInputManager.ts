8e6021ae5931bb6558a1c40ae3602df56e4847a6
Luke Chatton <chattonluke@gmail.com>
2026-01-14 17:30:28 -0800

    add flying

--- Assets/Code/Input/GameInputManager.ts
+++ Assets/Code/Input/GameInputManager.ts
@@ -1,4 +1,6 @@
 import { Airship } from "@Easy/Core/Shared/Airship";
+import { Game } from "@Easy/Core/Shared/Game";
+import { CoreAction } from "@Easy/Core/Shared/Input/AirshipCoreAction";
 import { Binding } from "@Easy/Core/Shared/Input/Binding";
 import { ActionId } from "./ActionId";
 import { SettingId } from "./SettingId";
@@ -20,6 +22,22 @@ export default class GameInputManager extends AirshipSingleton {
 		const globalVolume = GameObject.Find("GlobalVolume");
 		Airship.Settings.ObserveToggle(SettingId.PostProcessing, (val) => {
 			globalVolume.SetActive(val);
+		});
+	}
+
+	protected Start(): void {
+		if (Game.IsClient()) this.StartClient();
+	}
+
+	private StartClient() {
+		let lastJump = 0;
+		Airship.Input.OnDown(CoreAction.Jump).Connect((e) => {
+			if (Time.time - lastJump < 0.25) {
+				if (Game.localPlayer.character) {
+					Game.localPlayer.character.movement.SetFlying(!Game.localPlayer.character.movement.IsFlying());
+				}
+			}
+			lastJump = Time.time;
 		});
 	}
 }
