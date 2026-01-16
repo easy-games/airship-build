48aa5ed34b98f29a7d4a8acc58882d0336de7bb0
Luke Chatton <chattonluke@gmail.com>
2026-01-15 16:35:36 -0800

    add build perms to world cmd

--- Assets/Code/Command/WorldsCommand.ts
+++ Assets/Code/Command/WorldsCommand.ts
@@ -1,5 +1,6 @@
 import { ChatCommand } from "@Easy/Core/Shared/Commands/ChatCommand";
 import { Player } from "@Easy/Core/Shared/Player/Player";
+import { ChatColor } from "@Easy/Core/Shared/Util/ChatColor";
 import WorldManager from "Code/World/WorldManager";
 
 export default class WorldsCommand extends ChatCommand {
@@ -11,7 +12,11 @@ export default class WorldsCommand extends ChatCommand {
 		player.SendMessage("Worlds (Player, WorldID):");
 		let i = 1;
 		for (const world of WorldManager.Get().loadedWorlds) {
-			player.SendMessage(`  ${i}. ${world.GetOwnerPlayer()?.username ?? "Unknown"} ${world.worldId}`);
+			player.SendMessage(
+				`  ${i}. ${world.GetOwnerPlayer()?.username ?? "Unknown"} ${world.worldId}. Perms: ${
+					world.HasBuildPermission(player) || world.IsOwner(player) ? ChatColor.Yellow("Yes") : "No"
+				}`,
+			);
 			i++;
 		}
 	}
